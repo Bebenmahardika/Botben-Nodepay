f04ee8ab02df8dd58b8473afd570a3cf0ce55322
Bebenmahardika <90575411+Bebenmahardika@users.noreply.github.com>
2024-11-24 13:42:29 +0700

    Update install_bot.sh

--- install_bot.sh
+++ install_bot.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Membuat screen session dengan nama Nodepayben
-screen -S Nodepayben -d -m
-
-# Menjalankan perintah git clone dan mengakses folder
-screen -S Nodepayben -X stuff "git clone https://github.com/Bebenmahardika/Botben-Nodepay\n"
-screen -S Nodepayben -X stuff "cd Botben-Nodepay\n"
-screen -S Nodepayben -X stuff "python3 benskoy.py\n"
+screen -S Nodepayben
+git clone https://github.com/Bebenmahardika/Botben-Nodepay
+cd Botben-Nodepay
+python3 benskoy.py
